cd95c778b397d79b2d14ac77bc676915ed66ed50
alex2022-lab <111999591+alex2022-lab@users.noreply.github.com>
2024-04-23 15:43:46 -0400

    Taller realizado

---
+++ introduction/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Lista_tareas></Lista_tareas>
+</template>
+
+<script>
+import Lista_tareas from './components/Lista_tareas.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Lista_tareas
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ introduction/src/components/Lista_tareas.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        
+        <h1>Lista de Quehaceres</h1>
+        <ul>
+            <li v-for="tarea in tareas" :key="tarea.id">{{ tarea.descripcion }}</li>
+        </ul>
+        <input v-model="nuevaTarea" @keyup.enter="añadir" placeholder="Añadir nueva tarea">
+        <button @click="añadir">Agregar Tarea</button>
+        
+    </div>
+</template>
+
+<script>
+export default{
+    data() {
+        return {
+            tareas: [
+                {
+                    id: 1,
+                    descripcion: 'Limpiar la casa'
+                },
+                {
+                    id: 2,
+                    descripcion: 'Hacer el almuerzo'
+                }
+            ],
+            nuevaTarea: ''
+        };
+    },
+    methods: {
+        añadir() {
+      if (this.nuevaTarea.trim() !== '') {
+        const nuevaId = this.tareas.length + 1;
+        this.tareas.push({ id: nuevaId, descripcion: this.nuevaTarea });
+        this.nuevaTarea = '';
+      }
+    },
+ }
+    
+};
+
+</script>
+
